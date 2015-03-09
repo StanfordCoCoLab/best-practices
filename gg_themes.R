736d470dc662805e0c28e9fd78844931714246f9
mhtess <mht137@gmail.com>
2015-03-08 22:33:50 -0700

    Refine blackDisplay, add papers theme

--- gg_themes.R
+++ gg_themes.R
@@ -85,8 +85,8 @@ theme_blackDisplay <- function(base_size = 12, base_family = "Helvetica") {
     strip.text =         element_text(size = 30, colour = "white"),
     
     axis.line =          element_blank(),
-    axis.text.x =        element_text(size=20,vjust = 0.8),
-    axis.text.y =        element_text(size=20,hjust = 1),
+    axis.text.x =        element_text(size=30,vjust = 0.8),
+    axis.text.y =        element_text(size=30,hjust = 1),
     axis.ticks =         element_line(colour = "white", size = 0.2),
     axis.title =         element_text(size=32,colour = "white"),
     axis.title.x =       element_text(vjust = 1),
@@ -94,15 +94,15 @@ theme_blackDisplay <- function(base_size = 12, base_family = "Helvetica") {
     axis.ticks.length =  unit(0.3, "lines"),
     axis.ticks.margin =  unit(0.5, "lines"),
     
-    legend.background =  element_rect(colour = "white", fill='black'),
+    legend.background =  element_rect(colour = "black", fill='black'),
     legend.margin =      unit(0.2, "cm"),
-    legend.key =         element_rect(fill = "black", colour = "white"),
+    legend.key =         element_rect(fill = "black", colour = "black"),
     legend.key.size =    unit(1.2, "lines"),
-    legend.key.height =  NULL,
-    legend.key.width =   NULL,
-    legend.text =        element_text(size = 26, colour = "white"),
+    legend.key.height =  unit(3,"line"),
+    legend.key.width =   unit(3,"line"),
+    legend.text =        element_text(size = 30, colour = "white"),
     legend.text.align =  NULL,
-    legend.title =       element_text(size = 26, face = "bold", hjust = 0, colour = "white"),
+    legend.title =       element_text(size = 30, face = "bold", hjust = 0, colour = "white"),
     legend.title.align = NULL,
     legend.position =    "right",
     legend.direction =   "vertical",
@@ -110,9 +110,9 @@ theme_blackDisplay <- function(base_size = 12, base_family = "Helvetica") {
     legend.box =         NULL,
     
     panel.background =   element_rect(fill = "black", colour = NA),
-    panel.border =       element_rect(fill = NA, colour = "white"),
-    panel.grid.major =   element_line(colour = "grey20", size = 0.2),
-    panel.grid.minor =   element_line(colour = "grey10", size = 0.5),
+    panel.border =       element_rect(fill = NA, colour = "black"),
+    panel.grid.major =   element_line(colour = "black", size = 0.2),
+    panel.grid.minor =   element_line(colour = "black", size = 0.5),
     panel.margin =       unit(0.25, "lines"),
     panel.margin.x =       unit(0.25, "lines"),
     panel.margin.y =       unit(0.25, "lines"),
@@ -129,6 +129,64 @@ theme_blackDisplay <- function(base_size = 12, base_family = "Helvetica") {
     complete = TRUE
   )
 }
+
+
+theme_paper <- function(base_size = 12, base_family = "Helvetica") {
+  theme(
+    line =               element_line(colour = "black", size = 0.5, linetype = 1,
+                                      lineend = "butt"),
+    rect =               element_rect(fill = "black", colour = "black", size = 0.5, linetype = 1),
+    text =               element_text(family = base_family, face = "plain",
+                                      colour = "black", size = base_size,
+                                      hjust = 0.5, vjust = 0.5, angle = 0, lineheight = 0.9),
+    axis.text =          element_text(size = rel(0.8), colour = "black"),
+    strip.text =         element_text(size = rel(0.8), colour = "black"),
+    
+    axis.line =          element_blank(),
+    axis.text.x =        element_text(size=14,vjust = 0.8),
+    axis.text.y =        element_text(size=14,hjust = 1),
+    axis.ticks =         element_line(colour = "black", size = 0.2),
+    axis.title =         element_text(colour = "black"),
+    axis.title.x =       element_text(vjust = -0.5,size=16),
+    axis.title.y =       element_text(angle = 90,vjust=1,size=16),
+    axis.ticks.length =  unit(0.3, "lines"),
+    axis.ticks.margin =  unit(0.5, "lines"),
+    
+    legend.background =  element_rect(colour = "white", fill='white'),
+    legend.margin =      unit(0.2, "cm"),
+    legend.key =         element_rect(fill = "white", colour = "white"),
+    legend.key.size =    unit(1.2, "lines"),
+    legend.key.height =  NULL,
+    legend.key.width =   NULL,
+    legend.text =        element_text(size = rel(1.2), colour = "black"),
+    legend.text.align =  NULL,
+    legend.title =       element_text(size = rel(1.2), face = "bold", hjust = 0, colour = "black"),
+    legend.title.align = NULL,
+    legend.position =    "right",
+    legend.direction =   "vertical",
+    legend.justification = "center",
+    legend.box =         NULL,
+    
+    panel.background =   element_rect(fill = "white", colour = NA),
+    panel.border =       element_rect(fill = NA, colour = "black"),
+    panel.grid.major =   element_line(colour = "white", size = 0.2),
+    panel.grid.minor =   element_line(colour = "white", size = 0.5),
+    panel.margin =       unit(0.25, "lines"),
+    panel.margin.x =       unit(0.25, "lines"),
+    panel.margin.y =       unit(0.25, "lines"),
+    
+    strip.background =   element_rect(fill = "white", colour = "white"),
+    strip.text.x =       element_text(size=14),
+    strip.text.y =       element_text(angle = -90,size=14),
+    
+    plot.background =    element_rect(colour = "white", fill = "white"),
+    plot.title =         element_text(size = rel(1.2)),
+    plot.margin =        unit(c(1, 1, 0.5, 0.5), "lines"),
+    
+    complete = TRUE
+  )
+}
+
 
 theme_set(theme_black())
 
